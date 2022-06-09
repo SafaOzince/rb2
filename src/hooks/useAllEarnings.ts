2c499c53a838e4c94260faf4b9571751d2e391e9
SafaOzince <safabaturalpozince@gmail.com>
2022-06-09 15:31:10 +0300

    repoconfigs

--- src/hooks/useAllEarnings.ts
+++ src/hooks/useAllEarnings.ts
@@ -15,7 +15,7 @@ const useAllEarnings = () => {
     const fetchAllBalances = async () => {
       const calls = farmsConfig.map((farm) => ({
         address: getMasterChefAddress(),
-        name: 'pendingEgg',
+        name: 'pendingRobinia',
         params: [farm.pid, account],
       }))
 
--- src/hooks/useFarmsWithBalance.ts
+++ src/hooks/useFarmsWithBalance.ts
@@ -21,7 +21,7 @@ const useFarmsWithBalance = () => {
     const fetchBalances = async () => {
       const calls = farmsConfig.map((farm) => ({
         address: getMasterChefAddress(),
-        name: 'pendingEgg',
+        name: 'pendingRobinia',
         params: [farm.pid, account],
       }))
 
