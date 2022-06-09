4e818901a5e7248041bd1a75740862c034fa7e16
SafaOzince <safabaturalpozince@gmail.com>
2022-06-09 03:39:29 +0300

    contractaddressupd

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,15 +1,15 @@
 export default {
   cake: {
     56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
-    97: '',
+    97: '0xEbD1868E3c49b8eEfc0418d5Ca4dE1022b899d60',
   },
   masterChef: {
     56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
-    97: '',
+    97: '0x0Aa59aF054f60078B56051D52594f0785b0501dA',
   },
   wbnb: {
     56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
-    97: '',
+    97: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
   },
   lottery: {
     56: '',
@@ -25,6 +25,6 @@ export default {
   },
   busd: {
     56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-    97: '',
+    97: '0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7',
   },
 }
